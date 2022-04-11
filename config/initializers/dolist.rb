7b60c84f13c8109bead3c46c39a16bf8f4bb1f7f
simon lehericey <mail@simon.lehericey.net>
2022-04-11 17:22:32 +0200

    dolist config

---
+++ config/initializers/dolist.rb
@@ -0,0 +1,24 @@
+ActiveSupport.on_load(:action_mailer) do
+  module Dolist
+    class SMTP < ::Mail::SMTP
+      def deliver!(mail)
+        mail.from(ENV['DOLIST_NO_REPLY_EMAIL'])
+        mail.sender(ENV['DOLIST_NO_REPLY_EMAIL'])
+        mail['X-ACCOUNT-ID'] = Rails.application.secrets.dolist[:account_id]
+
+        super(mail)
+      end
+    end
+  end
+
+  ActionMailer::Base.add_delivery_method :dolist, Dolist::SMTP
+
+  ActionMailer::Base.dolist_settings = {
+    user_name: Rails.application.secrets.dolist[:username],
+    password: Rails.application.secrets.dolist[:password],
+    address: 'smtp.dolist.net',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
